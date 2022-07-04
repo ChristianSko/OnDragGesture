f222731fca0498b51e203369a33ae7ac44a5492b
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-07-04 08:17:15 +0200

    Initial Commit

---
+++ OnDragGesture/OnDragGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnDragGestureApp.swift
+//  OnDragGesture
+//
+//  Created by Skorobogatow, Christian on 4/7/22.
+//
+
+import SwiftUI
+
+@main
+struct OnDragGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
